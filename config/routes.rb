0004aca0bd657b37a0be5994fe1be7a8d8f29524
Ryu SeongHoon <4997174+ryush00@users.noreply.github.com>
2024-10-09 13:05:42 +0000

    feat: 루트 페이지 추가

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "application#index"
 end
