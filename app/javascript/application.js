d7aa8ab8de0c93fb4904a3467d4915431e58af92
Ryu SeongHoon <4997174+ryush00@users.noreply.github.com>
2024-11-02 05:06:29 +0000

    fix: 페이지 이동 후 turbo로 인해 창(backdrop)이 두번 올라오는 현상 수정

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,25 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('turbo:load', function() {
+  var triggers = document.querySelectorAll('.offcanvas-trigger');
+  
+  triggers.forEach(function(trigger) {
+    // 이미 리스너가 등록되었는지 확인
+    if (!trigger.dataset.listenerAdded) {
+      trigger.addEventListener('click', function(event) {
+        event.preventDefault(); // 기본 동작 방지
+        var targetId = trigger.getAttribute('href');
+        var offcanvasElement = document.querySelector(targetId);
+
+        if (offcanvasElement) {
+          var offcanvas = new bootstrap.Offcanvas(offcanvasElement);
+          offcanvas.show();
+        }
+      });
+      
+      trigger.dataset.listenerAdded = 'true'; // 리스너가 추가되었음을 표시
+    }
+  });
+});
