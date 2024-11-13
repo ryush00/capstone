4754d475c46fdf6fbce9a3d27d9a0dc3c71a03e8
Ryu SeongHoon <4997174+ryush00@users.noreply.github.com>
2024-11-13 02:30:56 +0000

    schema 버전 업

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_09_145709) do
+ActiveRecord::Schema[8.0].define(version: 2024_10_09_145709) do
   create_table "bookings", force: :cascade do |t|
     t.integer "pool_id", null: false
     t.integer "user_id", null: false
