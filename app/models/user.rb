7fdb9be31d9e37f3ca79a443c55b83768ea8b15e
SeongHoon Ryu <4997174+ryush00@users.noreply.github.com>
2024-11-20 00:21:23 +0000

    feat: 계좌 정보 암호화하도록 수정

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable,
          :trackable
+
+  encrypts :account_number, :account_bank, :account_name
 end
