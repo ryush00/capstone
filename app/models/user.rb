f5af505716681dd0dba611a0744efc378f88ce86
양진우 <144159898+Yang0Jin0Woo@users.noreply.github.com>
2024-11-23 11:40:32 +0000

    입금자명 한글만 가능

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ApplicationRecord
   validates :account_bank, inclusion: { in: BANK_NAMES, message: "은행명이 유효하지 않습니다" }, allow_blank: true
 
   # 한글만 허용
-  validates :account_name, format: { with: /\A[가-힣\s]+\z/, message: "입금자는 한글만 입력 가능합니다." }, allow_blank: true
+  validates :account_name, format: { with: /\A[가-힣]+\z/, message: "입금자는 한글만 입력 가능합니다." }, allow_blank: true
 
   # 숫자만 허용
   validates :account_number, format: { 
