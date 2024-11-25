b38b3f675435f90d27f6d84f35ed55b47097b24b
SeongHoon Ryu <4997174+ryush00@users.noreply.github.com>
2024-11-25 23:48:03 +0000

    feat: 이메일 주소 변경

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "carpool@ryush00.me"
+  config.mailer_sender = "carpool@wkcar.kr"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
