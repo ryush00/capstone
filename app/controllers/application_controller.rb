64ca6e1870cf154601dd166dc797c939e319218d
SeongHoon Ryu <4997174+ryush00@users.noreply.github.com>
2024-12-04 04:14:40 +0000

    HOTFIX: 예전 브라우저 접속 허용

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
 
   def index
     @pools = Pool.all
